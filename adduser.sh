f57ead2f836b2be70562e0479723a4b0681fe378
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 17:15:15 -0600

    Create adduser.sh

---
+++ adduser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+echo "What is the new user's name?"
+read user
+adduser $user --ingroup ftpaccess --shell /usr/sbin/nologin
+chown root:root /home/$user
+mkdir /home/$user/www
+chown $user:ftpaccess /home/$user/www
