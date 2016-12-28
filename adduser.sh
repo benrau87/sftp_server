e1412491e214b4b653e5151d083bfd222027baf1
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 21:27:17 -0600

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -4,10 +4,14 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
+expdate = 'date -d "7 days" +"%Y-%m-%d"'
 
 echo "What is the new user's name?"
 read user
-adduser -e `date -d "7 days" +"%Y-%m-%d"` $user --ingroup ftpaccess --shell /usr/sbin/nologin
+
+adduser $user -e $expdate --ingroup ftpaccess --shell /usr/sbin/nologin
 chown root:root /home/$user
 mkdir /home/$user/upload
 chown $user:ftpaccess /home/$user/upload
+
+
