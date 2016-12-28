1d62146edb00a87e4931a21c9253aac0104f8dd0
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 22:48:29 -0600

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -2,7 +2,16 @@
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+
+
+logfile=/var/log/sftp_users.log
+mkfifo ${logfile}.pipe
+tee < ${logfile}.pipe $logfile &
+exec &> ${logfile}.pipe
+rm ${logfile}.pipe
+
 expdate=$(date -d "7 days" +"%Y-%m-%d")
+pass=$(openssl rand -base64 16)
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -11,12 +20,12 @@ fi
 
 echo -e "${YELLOW} What is the new user's name?${NC}"
 read user
-
+echo $pass | passwd $user --stdin
 adduser $user --ingroup ftpaccess --shell /usr/sbin/nologin
 usermod $user --expiredate $expdate
 chown root:root /home/$user
 mkdir /home/$user/uploads
 chown $user:ftpaccess /home/$user/uploads
 
-echo -e "${YELLOW}User account ${RED}$user ${YELLOW}has been created and will expire on ${RED}$expdate ${NC}"
+echo -e "${YELLOW}User account ${RED}$user ${YELLOW}with password ${RED}$pass ${YELLOW}has been created and will expire on ${RED}$expdate ${NC}" &>> $logfile
 
