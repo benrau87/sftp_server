d228e3afcdea993dd8f2c8a782ee71120ccebf34
benrau87 <benrau87@users.noreply.github.com>
2016-12-27 21:30:30 -0600

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -6,7 +9,7 @@ if [ "$EUID" -ne 0 ]
 fi
 expdate = 'date -d "7 days" +"%Y-%m-%d"'
 
-echo "What is the new user's name?"
+echo -e "${YELLOW} What is the new user's name?${NC}"
 read user
 
 adduser $user -e $expdate --ingroup ftpaccess --shell /usr/sbin/nologin
@@ -14,4 +17,5 @@ chown root:root /home/$user
 mkdir /home/$user/upload
 chown $user:ftpaccess /home/$user/upload
 
+echo -e "${YELLOW}User account ${RED} $user ${YELLOW}has been created and will expire on ${RED} $expdate ${NC}"
 
