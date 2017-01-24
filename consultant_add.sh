45d83389f71830f5981acdd4423aaf69a9969d2e
benrau87 <benrau87@users.noreply.github.com>
2017-01-24 14:10:49 -0600

    Update consultant_add.sh

--- consultant_add.sh
+++ consultant_add.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
@@ -22,11 +21,15 @@ fi
 echo -e "${YELLOW} What is the consultants name?${NC}"
 read user
 
-adduser --ingroup ftpaccess --shell /usr/sbin/nologin --disabled-password --gecos ""  $user
+adduser --ingroup ftpaccess --disabled-password --gecos ""  $user
 echo $user:$pass | chpasswd
 usermod $user --expiredate $expdate
+chown root:root /home/$user
+mkdir /home/$user/uploads
+chown $user:ftpaccess /home/$user/uploads
 
 echo
 echo -e "${YELLOW}User account ${RED}$user ${YELLOW}with password ${RED}$pass ${YELLOW}has been created and will expire on ${RED}$expdate ${NC}" 
 
 echo "User account $user with password $pass will expire on $expdate" &>> $logfile
+
